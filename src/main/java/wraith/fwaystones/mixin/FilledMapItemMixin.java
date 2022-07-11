1932af93bc7ef43d058ab1efee8e829435fb8760
roko <40763333+rokoblox@users.noreply.github.com>
2022-07-12 01:47:57 +0200

    MixinExtras no more

--- src/main/java/wraith/fwaystones/mixin/FilledMapItemMixin.java
+++ src/main/java/wraith/fwaystones/mixin/FilledMapItemMixin.java
@@ -1,22 +1,24 @@
 package wraith.fwaystones.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyReceiver;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.FilledMapItem;
 import net.minecraft.item.map.MapState;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 import wraith.fwaystones.item.map.MapStateAccessor;
 
 @Mixin(FilledMapItem.class)
 public class FilledMapItemMixin {
-    @ModifyReceiver(method = "updateColors", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/map/MapState;removeBanner(Lnet/minecraft/world/BlockView;II)V"))
-    private MapState removeWaystones(MapState receiver, BlockView world, int x, int y) {
+
+    @ModifyArgs(method = "updateColors", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/map/MapState;removeBanner(Lnet/minecraft/world/BlockView;II)V"))
+    private void updateWaystoneMarkers(Args args, World world, Entity entity, MapState state) {
         BlockPos removalPos;
-        if ((removalPos = ((MapStateAccessor)receiver).removeWaystone(world, x, y)) != null)
-            ((MapStateAccessor)receiver).addWaystone((WorldAccess) world, removalPos);
-        return receiver;
+        if ((removalPos = ((MapStateAccessor)state).removeWaystone(world, args.get(1), args.get(2))) != null)
+            ((MapStateAccessor)state).addWaystone((WorldAccess) world, removalPos);
     }
 }
