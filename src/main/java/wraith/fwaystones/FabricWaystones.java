c68da0bdbd3bbb2c3b0d4d9bcc0d4def7b109913
LordDeatHunter <desuhansa@gmail.com>
2022-09-22 22:59:30 +0200

    Migrate old configs

--- src/main/java/wraith/fwaystones/FabricWaystones.java
+++ src/main/java/wraith/fwaystones/FabricWaystones.java
@@ -11,12 +11,23 @@ import wraith.fwaystones.util.WaystonePacketHandler;
 import wraith.fwaystones.util.WaystoneStorage;
 import wraith.fwaystones.util.WaystonesEventManager;
 
+import java.io.File;
+
 public class FabricWaystones implements ModInitializer {
 
-    public static final FWConfig CONFIG = FWConfig.createAndLoad();
+    public static final FWConfig CONFIG;
     public static final Logger LOGGER = LogManager.getLogger("Fabric-Waystones");
     public static final String MOD_ID = "fwaystones";
     public static WaystoneStorage WAYSTONE_STORAGE;
+
+    static {
+        var configFile = new File(FabricLoader.getInstance().getConfigDir().toFile(), "fwaystones/config.json");
+        if (configFile.exists()) {
+            FabricWaystones.LOGGER.info("Old config file found, migrating...");
+            configFile.renameTo(new File(FabricLoader.getInstance().getConfigDir().toFile(), "fwaystones/config.json5"));
+        }
+        CONFIG = FWConfig.createAndLoad();
+    }
 
     @Override
     public void onInitialize() {
