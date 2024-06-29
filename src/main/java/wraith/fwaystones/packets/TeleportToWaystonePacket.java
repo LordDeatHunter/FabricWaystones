abca1cdb0daf75324d3abf73314d41403a2e6112
IxPrumxI <walidaqeel@gmail.com>
2024-06-29 20:21:37 +0300

    mixed up the codec oops

--- src/main/java/wraith/fwaystones/packets/TeleportToWaystonePacket.java
+++ src/main/java/wraith/fwaystones/packets/TeleportToWaystonePacket.java
@@ -7,15 +7,14 @@ import net.minecraft.network.codec.PacketCodecs;
 import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.StringIdentifiable;
-import net.minecraft.util.Uuids;
 import wraith.fwaystones.FabricWaystones;
 import wraith.fwaystones.util.TeleportSources;
 
 public record TeleportToWaystonePacket(String waystone, String source) implements CustomPayload {
     public static final Id PACKET_ID = new Id<>(Identifier.of(FabricWaystones.MOD_ID, "teleport_to_waystone"));
     public static final Codec<TeleportToWaystonePacket> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-            StringIdentifiable.EnumCodec.STRING.fieldOf("source").forGetter(TeleportToWaystonePacket::source),
-            Codec.STRING.fieldOf("waystone").forGetter(TeleportToWaystonePacket::waystone)
+            Codec.STRING.fieldOf("waystone").forGetter(TeleportToWaystonePacket::waystone),
+            StringIdentifiable.EnumCodec.STRING.fieldOf("source").forGetter(TeleportToWaystonePacket::source)
     ).apply(instance, TeleportToWaystonePacket::new));
     public static final PacketCodec PACKET_CODEC = PacketCodecs.registryCodec(CODEC);
     public Id getId() {
