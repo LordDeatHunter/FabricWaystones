cd08672bfc4436daf21db38ec4a86fa0b630c4c8
Dragon-Seeker <timekeeperguild@gmail.com>
2025-06-07 19:10:16 -0500

    Prevent equip From Use for compat items

--- src/main/java/wraith/fwaystones/integration/accessories/AccessoriesCompat.java
+++ src/main/java/wraith/fwaystones/integration/accessories/AccessoriesCompat.java
@@ -2,8 +2,11 @@ package wraith.fwaystones.integration.accessories;
 
 import io.wispforest.accessories.api.AccessoriesAPI;
 import io.wispforest.accessories.api.AccessoriesCapability;
+import io.wispforest.accessories.api.Accessory;
+import net.minecraft.item.ItemStack;
 import wraith.fwaystones.api.WaystoneInteractionEvents;
 import wraith.fwaystones.api.core.ExtendedStackReference;
+import wraith.fwaystones.registry.WaystoneItems;
 
 public class AccessoriesCompat {
     public static void init() {
@@ -24,5 +27,16 @@ public class AccessoriesCompat {
 
             return null;
         });
+
+        var accessory = new Accessory() {
+            @Override
+            public boolean canEquipFromUse(ItemStack stack) {
+                return false;
+            }
+        };
+        AccessoriesAPI.registerAccessory(WaystoneItems.get("local_void"), accessory);
+        AccessoriesAPI.registerAccessory(WaystoneItems.get("void_totem"), accessory);
+        AccessoriesAPI.registerAccessory(WaystoneItems.get("pocket_wormhole"), accessory);
+        AccessoriesAPI.registerAccessory(WaystoneItems.get("abyss_watcher"), accessory);
     }
 }
